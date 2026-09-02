fd0053b4889d9cf16cc926aa0135f60a178bac2e
TrafficLight6 <TrafficLight6@outlook.com>
2026-09-02 22:57:27 +0800

    Edit: ConnectDB Logger Level 	modified:   sql/connect.go

--- sql/connect.go
+++ sql/connect.go
@@ -2,10 +2,14 @@ package sqlOperator
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/TrafficLight6/GoAccountHub/config"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectDB(appConfig config.Config) *gorm.DB {
@@ -17,7 +21,16 @@ func ConnectDB(appConfig config.Config) *gorm.DB {
 		appConfig.DatabasePassword,
 		appConfig.DatabaseName,
 	)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{Logger: newLogger})
 	if err != nil {
 		fmt.Println("Error in Connecting DB:", err)
 		return nil
