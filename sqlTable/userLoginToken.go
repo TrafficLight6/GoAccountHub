340625debf07e77f38f2d07083b4df3360c55ae5
TrafficLight6 <TrafficLight6@outlook.com>
2026-09-03 11:11:09 +0800

    Edit UserLoginToken table

--- sqlTable/userLoginToken.go
+++ sqlTable/userLoginToken.go
@@ -8,8 +8,9 @@ import (
 
 type UserLoginToken struct {
 	gorm.Model
-	ID       uint      `gorm:"primaryKey"`
-	UUHash   string    `gorm:"column:uu_hash"`
-	Token    string    `gorm:"column:token"`
-	DeadTime time.Time `gorm:"column:dead_time"`
+	ID              uint      `gorm:"primaryKey"`
+	UUHash          string    `gorm:"column:uu_hash"`
+	CharacterUUHash string    `gorm:"column:character_uu_hash"`
+	Token           string    `gorm:"column:token"`
+	DeadTime        time.Time `gorm:"column:dead_time"`
 }
