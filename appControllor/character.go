b661837a0ba68a6b3701a4df1f999e17bbee0dcd
TrafficLight6 <TrafficLight6@outlook.com>
2026-09-07 12:00:00 +0800

    Rename controllor, using alias

---
+++ appControllor/character.go
@@ -0,0 +1,9 @@
+package appControllor
+
+import "github.com/TrafficLight6/GoAccountHub/adminControllor"
+
+var CharacterAdd = adminControllor.CharacterAdd
+var CharacterDelete = adminControllor.CharacterDelete
+var CharacterEdit = adminControllor.CharacterEdit
+var CharacterGet = adminControllor.CharacterGet
+var CharacterRange = adminControllor.CharacterRange
