c2e58bac62eb25fc7b7331b3896bcf7943dcef94
TrafficLight6 <TrafficLight6@outlook.com>
2026-09-10 09:55:23 +0800

    Fix: change admin login token getting logic

--- adminControllor/adminLogout.go
+++ adminControllor/adminLogout.go
@@ -8,30 +8,28 @@ import (
 	"gorm.io/gorm"
 )
 
-type AdminLogoutRequest struct {
-	Token string `json:"token"`
-}
-
 func AdminLogout(c *gin.Context) {
-	var body AdminLogoutRequest
-	if err := c.ShouldBindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "error": "Invalid request body"})
+	//Get cookie
+	token, err := c.Cookie("admin_token")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "error": "Token is empty"})
 		return
 	}
+
 	//Validate Token
-	if body.Token == "" {
+	if token == "" {
 		c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "error": "Token is empty"})
 		return
 	}
 	//Get db
 	db := c.Value("db").(*gorm.DB)
 	//Check Token Exist
-	if !sqlOperator.CheckAdminToken(db, body.Token) {
+	if !sqlOperator.CheckAdminToken(db, token) {
 		c.JSON(http.StatusUnauthorized, gin.H{"code": http.StatusUnauthorized, "error": "Token not found"})
 		return
 	}
 	//Delete Token From Database(Logout)
-	if err := sqlOperator.DelAdminToken(db, body.Token); err != nil {
+	if err := sqlOperator.DelAdminToken(db, token); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "error": "Internal server error"})
 		return
 	}
