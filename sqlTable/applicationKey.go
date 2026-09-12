61830f6338a2ed61a1f0fe7ec4613c9e256ed871
TrafficLight6 <TrafficLight6@outlook.com>
2026-09-12 22:57:58 +0800

    Change field name

--- sqlTable/applicationKey.go
+++ sqlTable/applicationKey.go
@@ -6,6 +6,6 @@ type ApplicationKey struct {
 	gorm.Model
 	ID int64 `gorm:"primaryKey"`
 	//UNIQUE
-	ApplicationUser string `gorm:"column:application_user"`
-	Key             string `gorm:"column:key"`
+	KeyUser string `gorm:"column:application_user"`
+	Key     string `gorm:"column:key"`
 }
