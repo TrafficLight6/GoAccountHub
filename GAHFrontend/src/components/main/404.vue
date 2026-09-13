cafe4153aaa3cca5ea09b7391dba9b277d465357
TrafficLight6 <TrafficLight6@outlook.com>
2026-09-13 16:55:33 +0800

    Add 404 page

---
+++ GAHFrontend/src/components/main/404.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="not-found">
+        <h1>404</h1>
+        <p>The page you are looking for does not exist.</p>
+        <el-button type="primary" @click="goHome">Back to Home</el-button>
+    </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goHome = () => {
+    router.push('/main/home')
+}
+</script>
+
+<style scoped>
+.not-found {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    background-color: #f5f7fa;
+}
+
+.not-found h1 {
+    margin: 0;
+    font-size: 72px;
+    color: #303133;
+}
+
+.not-found p {
+    margin: 0 0 16px;
+    color: #606266;
+}
+</style>
